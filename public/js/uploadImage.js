448da5036baf68a619a0f7ca078004bf962af431
Jerry Davis <jerry.davis@smail.astate.edu>
2020-05-12 17:30:30 -0500

    /

--- public/js/uploadImage.js
+++ public/js/uploadImage.js
@@ -1,18 +1,20 @@
 //Function that takes the user uploaded image file, then stores it as a 64bit url I think
 async function uploadImage()
 {
-    var data = 
+    /*var data = 
     {
         title: document.getElementById('title').value,
         image: document.getElementById('file').files[0]
-    }
+    }*/
+    var data = new FormData();
+    data.append(title, document.getElementById('title').value);
+    data.append(image, document.getElementById('file').files[0])
 
     //Call path to post the saved data to the database
     try {
         const res = await fetch('http://40.122.146.213/gallery', {
             method: 'POST',
-            body: JSON.stringify(data),
-            headers: {"Content-Type": "application/json"}
+            body: JSON.stringify(data);
         });
         console.log(`Res.status = ${res.status}`)
         if (res.status === 403) {
