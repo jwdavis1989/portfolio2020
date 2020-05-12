4e32a7685cc5b4c48edf2f01ff6ca945761ee30a
Jerry Davis <jerry.davis@smail.astate.edu>
2020-05-12 17:37:33 -0500

    /

--- public/js/uploadImage.js
+++ public/js/uploadImage.js
@@ -9,14 +9,14 @@ async function uploadImage()
     var data = new FormData();
     const tempImage = document.getElementById('file').files[0];
     console.log(`TempImage = ${tempImage}`);
-    data.append(title, document.getElementById('title').value);
-    data.append(image, tempImage);
+    data.append("title", document.getElementById('title').value);
+    data.append("image", tempImage);
 
     //Call path to post the saved data to the database
     try {
         const res = await fetch('http://40.122.146.213/gallery', {
             method: 'POST',
-            body: JSON.stringify(data)
+            body: data
         });
         console.log(`Res.status = ${res.status}`)
         if (res.status === 403) {
